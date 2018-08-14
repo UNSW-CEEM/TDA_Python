4dcd4e06580301476340ccb1d88e317cff050284
Nick <n.gorman305@gmail.com>
2018-08-14 13:45:29 +1000

    graph working

--- tda.py
+++ tda.py
@@ -12,7 +12,7 @@ import pandas as pd
 
 class Tda(QMainWindow):
     def __init__(self):
-        # Load in info needed at start to create gui, e.g Tariff types
+        # Provide default values.
         self.data_folder = os.getcwd() + '/data'
         self.mapping_file = 'demographics_load_lookup_table.csv'
 
@@ -23,18 +23,18 @@ class Tda(QMainWindow):
         self.gridlayout = QGridLayout()
         self.centralWidget.setLayout(self.gridlayout)
 
-        #
+        # Create variables to store data tables and currently selected load.
         self.demographic_tables = {}
         self.load_tables = {}
-        self. selected_load = None
+        self.selected_load = None
 
-        # Create subsection of gui, and pass them any information needed.
+        # Create subsections of gui.
         self.init_menu()
         self.gridlayout.addWidget(Logo('CEEMLogo.png'), 0, 0, 1, 1)
         self.gridlayout.addWidget(Logo('UNSWLogo.png'), 0, 1, 1, 1)
-        self.load_selection = LoadSelectionPanel('Select Load', self.set_load_data, self.get_load_options,
-                                                 self.get_demographics_options, self.update_demographic_data,
-                                                 self.give_load)
+        self.load_selection = \
+            LoadSelectionPanel('Select Load', self.give_load, self.get_load_options, self.get_demographics_options,
+                               self.update_demographic_data, self.set_load_data)
         self.gridlayout.addWidget(self.load_selection, 1, 0, 2, 2)
         self.tariff_select_panel = TariffSelectionPanel('Select Tariff')
         self.gridlayout.addWidget(self.tariff_select_panel, 2, 2, 1, 1)
@@ -54,10 +54,14 @@ class Tda(QMainWindow):
         project.addAction(save_project)
 
     def set_load_data(self, load_data_name, criteria):
+        # Check if the full set of load data has been loaded, if not load the data.
         if load_data_name not in self.load_tables:
             self.load_tables[load_data_name] = get_load_table(self.data_folder, load_data_name)
+        # Get the demographic data for the particular load table.
         all_load_ids = self.demographic_tables[load_data_name]
+        # Choose a subset of the demographic based on the criteria provided.
         demographic_subset = all_load_ids.loc[(all_load_ids[list(criteria)] == pd.Series(criteria)).all(axis=1)]
+        # Use the IDs of the subset to select the loads that we need and set this load subset as the selected load.
         just_ids = demographic_subset['HomeID']
         whole_load_table = self.load_tables[load_data_name]
         self.selected_load = whole_load_table.loc[:, ('TimeStamp',) + tuple(just_ids)]
@@ -66,14 +70,18 @@ class Tda(QMainWindow):
         return self.selected_load
 
     def get_load_options(self):
+        # Find the list of load files that users can select.
         return extract_load_options(self.data_folder)
 
     def update_demographic_data(self, new_load_data_name):
+        # If the demographic data for a load table has not be loaded, then load it and store by reference to the load
+        # table.
         if new_load_data_name not in self.demographic_tables:
             self.demographic_tables[new_load_data_name] = get_demographics_table(self.data_folder, new_load_data_name,
                                                                                  self.mapping_file)
 
     def get_demographics_options(self, load_data_name):
+        # Find the demographic information that users can filter on and the potential filtering values.
         return extract_demographics_options(self.demographic_tables[load_data_name])
 
 
@@ -87,27 +95,31 @@ class Logo(QLabel):
 
 
 class LoadSelectionPanel(QGroupBox):
-    def __init__(self, name, update_load_function, get_load_options, get_demographics_options, update_demographic_data,
-                 get_load_function):
+    def __init__(self, name, get_load, get_load_options, get_demographics_options, update_demographic_data,
+                 update_load_function):
         super().__init__(name)
 
+        # Label the load selection bar.
         select_label = QLabel('Select:')
+
+        # Create the main load selection bar.
         self.load_list = QComboBox()
         self.load_list.setMinimumWidth(100)
-        load_options = get_load_options()
         self.load_list.addItem('None')
-        for file in load_options:
+        for file in get_load_options():
             self.load_list.addItem(file)
-            self.load_list.currentIndexChanged.connect(
+        # Connect a change in the load bar selection to function that updates the demographic filtering options.
+        self.load_list.currentIndexChanged.connect(
             lambda: self.update_demographics_options(self.load_list.currentText(),update_demographic_data,
-                                                     get_demographics_options))
+                                                 get_demographics_options))
 
-        self.update_load_function = update_load_function
-
+        # Create a button that set the selected load based on input from the load selection bar and the demographic
+        # options.
         set_button = QPushButton('Set')
-        set_button.clicked.connect(lambda: self.update_active_load_data(update_load_function, get_load_function))
+        set_button.clicked.connect(lambda: self.update_active_load_data(update_load_function, get_load))
         set_button.setFixedWidth(50)
 
+        # Place the load selection widgets in a horizontal bar layout.
         self.top_bar = QHBoxLayout()
         self.top_bar.addWidget(select_label,  alignment=QtCore.Qt.AlignLeft)
         self.top_bar.addWidget(self.load_list, alignment=QtCore.Qt.AlignLeft)
@@ -115,17 +127,20 @@ class LoadSelectionPanel(QGroupBox):
         self.top_bar.addWidget(set_button)
         self.top_bar.setAlignment(QtCore.Qt.AlignTop)
 
-        Separador = QFrame()
-        Separador.setFrameShape(QFrame.HLine)
-        Separador.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Expanding)
-        Separador.setLineWidth(1)
-        Separador.setFrameShadow(QFrame.Sunken)
+        # Visual separate the load selection bar and the demographic selections with a line.
+        separating_bar = QFrame()
+        separating_bar.setFrameShape(QFrame.HLine)
+        separating_bar.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Expanding)
+        separating_bar.setLineWidth(1)
+        separating_bar.setFrameShadow(QFrame.Sunken)
 
+        # Create a mostly blank layout for the demographic selections, this get filled later once a load is selected.
         self.demographics = QVBoxLayout()
         self.demographics.insertWidget(0, QLabel('Select user group based on demongraphic info:'))
         self.demographics.addStretch(1)
         self.demographics.rows = []
 
+        # Create a box to contain the load plot.
         self.plot_group = QGroupBox()
         self.plot_group.setFlat(True)
         self.plot = tda_graphs.LoadPlot('Annual Average Profile', None)
@@ -138,45 +153,55 @@ class LoadSelectionPanel(QGroupBox):
         self.plot_group.header_layout.addWidget(self.plot_group.user_selected_label)
         self.plot_group.header_layout.addWidget(self.plot_group.show_label)
         self.plot_group.header_layout.addWidget(self.plot_group.plot_slection)
+        # Create a vertical layout in group box and place the header layout at the top and the plot below.
         self.plot_group.plot_group_layout = QVBoxLayout()
         self.plot_group.plot_group_layout.addLayout(self.plot_group.header_layout)
         self.plot_group.plot_group_layout.addWidget(self.plot)
         self.plot_group.setLayout(self.plot_group.plot_group_layout)
 
+        # Added the sub components to the main layout of the load selection panel.
         layout = QVBoxLayout()
         layout.addLayout(self.top_bar, 0)
-        layout.addWidget(Separador, 0)
+        layout.addWidget(separating_bar, 0)
         layout.addLayout(self.demographics, 1)
         layout.addWidget(self.plot_group, 1)
         layout.setAlignment(QtCore.Qt.AlignTop)
-
         self.setLayout(layout)
+
+        # Give the panel a maximum width.
         self.setMaximumWidth(500)
 
     def update_demographics_options(self, load_data_name, update_demographic_data, get_demographics_options):
+        # Clear any previous options.
+        for row in self.demographics.rows:
+            self.clearLayout(row)
+            self.demographics.removeItem(row)
+
+        # If the option selected is not the None option then create the relevant demographic selection boxes.
         self.demographics.comboboxes = {}
         if load_data_name != 'None':
+            # Make sure we have the demographic data for the load selected.
             update_demographic_data(load_data_name)
+            # Get the options for the demographic data.
             demographic_options = get_demographics_options(load_data_name)
+            # For each demographic data type add a combo box with all its possible values.
             for data_type, possible_values in demographic_options.items():
                 label = QLabel(data_type)
-                values_options = QComboBox()
-                values_options.addItem('All')
+                select_list = QComboBox()
+                select_list.addItem('All')
                 for value in possible_values:
-                    values_options.addItem(str(value))
+                    select_list.addItem(str(value))
                 row = QHBoxLayout()
-                self.demographics.comboboxes[data_type] = values_options
+                # Store reference to the selection boxes by name so they can be accessed later.
+                self.demographics.comboboxes[data_type] = select_list
                 row.addWidget(label)
-                row.addWidget(values_options)
+                row.addWidget(select_list)
                 row.addSpacing(50)
                 self.demographics.rows.append(row)
                 self.demographics.insertLayout(self.demographics.count()-1, row)
-        else:
-            for row in self.demographics.rows:
-                self.clearLayout(row)
-                self.demographics.removeItem(row)
 
     def get_demographic_selection_criteria(self):
+        # Read in the criteria from selection boxes.
         selection_criteria = {}
         for data_type, combo_box in self.demographics.comboboxes.items():
             if combo_box.currentText() != 'All':
@@ -184,14 +209,17 @@ class LoadSelectionPanel(QGroupBox):
         return selection_criteria
 
     def update_active_load_data(self, update_load_function, get_load_function):
+        # Get selection criteria.
         selection_criteria = self.get_demographic_selection_criteria()
+        # Update the selected load.
         update_load_function(self.load_list.currentText(), selection_criteria)
-        #self.plot.types[self.plot_group.plot_slection.currentText()](get_load_function)
+        # Delete the current plot.
         self.plot.deleteLater()
         self.plot = tda_graphs.LoadPlot(self.plot_group.plot_slection.currentText(), get_load_function)
         self.plot_group.plot_group_layout.addWidget(self.plot)
 
     def clearLayout(self, layout):
+        # Clear the widgets from a layout.
         while layout.count():
             child = layout.takeAt(0)
             if child.widget():
--- tda_graphs.py
+++ tda_graphs.py
@@ -3,40 +3,47 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from PyQt5.QtWidgets import QSizePolicy
 from numpy import arange, sin, pi
 import pandas as pd
+import pyqtgraph
+import numpy as np
+import matplotlib.dates as mdates
+
 
 class StandardCanvas(FigureCanvas):
-    """Ultimately, this is a QWidget (as well as a FigureCanvasAgg, etc.)."""
-
     def __init__(self, parent=None, width=5, height=4, dpi=100):
         fig = Figure(figsize=(width, height), dpi=dpi)
         self.axes = fig.add_subplot(111)
-
-        #self.compute_initial_figure()
-
         FigureCanvas.__init__(self, fig)
         self.setParent(parent)
-
         FigureCanvas.setSizePolicy(self, QSizePolicy.Expanding, QSizePolicy.Expanding)
         FigureCanvas.updateGeometry(self)
 
 
 class DualVariablePlot(StandardCanvas):
-    """Simple canvas with a sine plot."""
+    def __init__(self):
+        super().__init__()
+        self.compute_initial_figure()
 
     def compute_initial_figure(self):
-        t = arange(0.0, 3.0, 0.01)
+        t = arange(0.0, 3.0, 0.00001)
         s = sin(2 * pi * t)
         self.axes.plot(t, s)
 
 
 class LoadPlot(StandardCanvas):
-    """Simple canvas with a sine plot."""
     def __init__(self, type, get_data_function):
         super().__init__()
+        # Create all the pot features that are the same for every plot.
         self.types = {'Annual Average Profile': self.annual_average}
         self.data = None
+        # If a real plot type is given then create that plot.
+       # self.compute_initial_figure()
         if get_data_function is not None:
             self.types[type](get_data_function)
+
+    def compute_initial_figure(self):
+        t = arange(0.0, 3.0, 0.00001)
+        s = sin(2 * pi * t)
+        self.axes.plot(t, s)
 
     def annual_average(self, get_data_function):
         data = get_data_function()
@@ -46,5 +53,35 @@ class LoadPlot(StandardCanvas):
             print('grouping')
             self.data = self.data.groupby('TimeStamp', as_index=False).mean()
             print('ploting')
-            self.axes.plot(self.data['TimeStamp'].iloc[:], self.data['Load'].iloc[:])
+            self.data['TimeStamp'] = pd.to_datetime(self.data['TimeStamp'], format='%d/%m/%Y %H:%M')
+            self.data = self.data.sort_values('TimeStamp')
+            self.axes.plot(self.data['TimeStamp'], self.data['Load'])
+            self.axes.xaxis.set_major_locator(mdates.MonthLocator(interval=2))
+            self.axes.xaxis.set_major_formatter(mdates.DateFormatter('%Y/%m'))
+            self.axes.tick_params(labelsize=7)
             print('done')
+
+
+class LoadPlot2():
+    def __init__(self, type, get_data_function):
+        # Create all the pot features that are the same for every plot.
+        self.types = {'Annual Average Profile': self.annual_average,
+                      'Blank': self.blank}
+        self.data = None
+        # If a real plot type is given then create that plot.
+        if get_data_function is not None:
+            self.types[type](get_data_function)
+
+    def blank(self):
+        x = np.asarray([])
+        y = np.asarray([])
+        return pyqtgraph.plot(x, y, pen=None, symbol='o')
+
+    def annual_average(self, get_data_function):
+        data = get_data_function()
+        load_ids = [column for column in data.columns if column != 'TimeStamp']
+        data = pd.melt(data, id_vars='TimeStamp', value_vars=load_ids, var_name='IDs', value_name='Load')
+        data = data.groupby('TimeStamp', as_index=False).mean()
+        x = np.asarray(data['TimeStamp'])
+        y = np.asarray(data['Load'])
+        return pyqtgraph.plot(x, y, pen=None, symbol='o')
