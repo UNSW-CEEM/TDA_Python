0b75958032cbbb0e2c3bed4d47080e2fa1f60357
Nick <n.gorman305@gmail.com>
2019-08-30 16:49:55 +1000

    just export to excel

--- static/export_chart_data.js
+++ static/export_chart_data.js
@@ -1,18 +1,23 @@
 
 $( ".chart" ).contextmenu(event, function() {
     event.preventDefault();
-    ask_for_export_type($(event.target).closest("div[id]").attr('id'))
+    ask_for_export_type(event)
 });
 
-var ask_for_export_type = function(chart_id){
+var ask_for_export_type = function(event){
+    var chart_id = $(event.target).closest("div[id]").attr('id')
     $( "#message_dialog" ).dialog({
         modal: true,
-        width: 500,
-        buttons: {"Copy to clipboard": function(){copy_data(chart_id)},
-                  "Export to csv": function(){}}
+        buttons: {"Export": function(){export_chart_to_excel(chart_id)},
+                  "Cancel": function(){$( "#message_dialog" ).close()}},
+        position: {my: 'left', at: 'right', of: event}
     });
 };
 
 var copy_data = function(chart_id){
-    $( "#message_dialog" ).text(chart_id)
+    ;
 };
+
+var export_chart_to_excel = function(chart_id){
+    var chart_data = document.getElementById(chart_id).data
+}
