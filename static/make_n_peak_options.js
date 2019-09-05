51a375ba4eeabcb6ef1be943c70c8a4c36ed0d01
Nick <n.gorman305@gmail.com>
2019-09-05 12:14:30 +1000

    added in master version of make n peak options

--- static/make_n_peak_options.js
+++ static/make_n_peak_options.js
@@ -1,21 +1,28 @@
 // Add the possible number of peaks to the n= drop down menu
 for (i = 1; i <= 150; i++) {
-  var o = new Option("option text", i);
+  var o = new Option("option text", "value");
   $(o).html(i.toString());
   $("#x_n_peaks_select").append($(o).clone());
   $("#y_n_peaks_select").append($(o).clone());
 }
 
+// Update the visibility of the n peaks options.
+var control_options_display = function(axis){
+    var options = $('.' + axis + "_peak_options");
+    var axis_type = $('#dual_variable_' + axis + '_axis').children("option:selected").val();
+    if ( axis_type == 'avg_demand_n_peaks'){
+      options.css('visibility', 'visible');;
+    } else {
+      options.css('visibility', 'hidden');;
+    }
+}
 
 // Update the visibility of the n peaks options.
 var control_options_block_display = function(){
     var options = $("#peak_options_container");
     var x_axis = $('#dual_variable_x_axis').children("option:selected").val();
     var y_axis = $('#dual_variable_y_axis').children("option:selected").val();
-    if ( x_axis == 'avg_demand_n_peaks'|| y_axis == 'avg_demand_n_peaks' || 
-    x_axis == 'avg_demand_n_monthly_peaks' || y_axis == 'avg_demand_n_monthly_peaks' || 
-    x_axis == 'avg_demand_top_n_peaks'|| y_axis == 'avg_demand_top_n_peaks' || 
-    x_axis == 'avg_demand_top_n_monthly_peaks' || y_axis == 'avg_demand_top_n_monthly_peaks'){
+    if ( x_axis == 'avg_demand_n_peaks'|| y_axis == 'avg_demand_n_peaks'){
       options.show();
     } else {
       options.hide();
