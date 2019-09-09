26599ab13853b5455c84a61d0adc2598c2902c24
Nick <n.gorman305@gmail.com>
2019-09-09 14:11:39 +1000

    fixed chart export bug

--- format_chart_data_for_export.py
+++ format_chart_data_for_export.py
@@ -3,16 +3,27 @@ import pandas as pd
 
 def plot_ly_to_pandas(plot_ly_format_data):
     series_list = []
-    x_title = plot_ly_format_data['x_title']
-    y_title = plot_ly_format_data['y_title']
+    titles = {}
+    if 'x_title' in plot_ly_format_data.keys():
+        titles['x'] = plot_ly_format_data['x_title']
+        titles['y'] = plot_ly_format_data['y_title']
+    columns = []
     for series in plot_ly_format_data['chart_data']:
         series_dataframe = pd.DataFrame()
-        series_dataframe[x_title] = series['x']
-        series_dataframe[y_title] = series['y']
+        for axis in ['x', 'y', 'labels', 'values']:
+            if axis in series.keys():
+                if axis in titles.keys():
+                    title = titles[axis]
+                else:
+                    title = axis
+                series_dataframe[title] = series[axis]
+                if title not in columns:
+                    columns.append(title)
         if 'name' in series.keys():
             series_dataframe['Series'] = series['name']
+            if 'Series' not in columns:
+                columns.insert(0, 'Series')
         series_list.append(series_dataframe)
     export_dataframe = pd.concat(series_list, ignore_index=True)
-    if 'Series' in export_dataframe.columns:
-        export_dataframe = export_dataframe.loc[:, ['Series', x_title, y_title]]
+    export_dataframe = export_dataframe.loc[:, columns]
     return export_dataframe
