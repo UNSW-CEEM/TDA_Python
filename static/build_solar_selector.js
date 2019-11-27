89d503bebf8297a41db3dd40e2fdd3773739d2a7
Nick <n.gorman305@gmail.com>
2019-11-28 09:36:06 +1100

    minor changes to solar profiles selector

--- static/build_solar_selector.js
+++ static/build_solar_selector.js
@@ -1,6 +1,5 @@
 var add_solar_names = function(data){
     $.getJSON("/solar_names", function(json){
-
         $('#solar_data').empty();
         $.each(json, function(i, obj){
             $('#solar_data').append($('<option>').text(obj));
